de44e5336e1b6af2cbcbda18d215b07194fd6023
mn1del <davidleech@gmail.com>
2019-01-01 20:38:39 +0000

    debug hx711

--- hx711.py
+++ hx711.py
@@ -84,7 +84,7 @@ class HX711():
             self.EXTRA_PULSES = 3
         else:
             self.EXTRA_PULSES = 1
-        print("Pulses: {}".formatself.EXTRA_PULSES))
+        print("Pulses: {}".format(self.EXTRA_PULSES))
 
     def start_monitoring(self, printout=True):
         try:
