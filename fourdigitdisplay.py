23bb57db673e961638ed34aa0078a330b94bed11
mn1del <davidleech@gmail.com>
2019-01-01 17:52:18 +0000

    add hx711 and 4digitdisplay examples

---
+++ fourdigitdisplay.py
@@ -0,0 +1,85 @@
+import RPi.GPIO as GPIO
+import time
+
+
+class Display4s7s():
+    def __init__(
+            self,
+            segment_pins=(2, 3, 4, 17, 27, 22, 10, 9),
+            digit_pins=(5, 6, 13, 19)):
+        """
+        4 Digit, 7 Segment display
+
+        args:
+            segment_pins: (tuple(ints)). output pins to control digit segments (BCM)
+                    ** order must be bl, bm, dot, br, mid, tm, tl, tr
+            digit_pins: (tuple(ints)). output pins to control which digit to control
+        """
+        GPIO.set_mode(GPIO.BCM)
+        self.segment_pins = segment_pins
+        self.digit_pins = digit_pins
+        self.setup_pinouts(segment_pins)
+        self.setup_pinouts(digit_pins)
+        self.define_segment_map()
+        self.define_number_map()
+
+    def setup_pinouts(self):    
+        """
+        Setup pins as GPIO.out
+        args:
+            pins: tuple(int))
+        """
+        for pin in self.segment_pins:
+            GPIO.setup(pin, GPIO.OUT, initial=GPIO.LOW)
+
+        for pin in self.digit_pins:    
+            GPIO.setup(pin, GPIO.OUT, initial=GPIO.HIGH)
+
+    def define_segment_map(self):
+        """
+        define dictionary of segment pin mappings
+        {key: {display_pin: <x>, bcm_pin: <y>}}
+        """
+        self.segment_map = {
+                "bl": {"display_pin": 1, "bcm_pin": self.segment_pins[0]},  # bottom left segment
+                "bm": {"display_pin": 2, "bcm_pin": self.segment_pins[1]},  # bottom middle segment
+                "dot": {"display_pin": 3, "bcm_pin": self.segment_pins[2]},  # decimal point segment
+                "br": {"display_pin": 4, "bcm_pin": self.segment_pins[3]},  # bottom right segment
+                "mid": {"display_pin": 5, "bcm_pin": self.segment_pins[4]},  # middle segment
+                "tm": {"display_pin": 11, "bcm_pin": self.segment_pins[5]},  # top middle segment
+                "tl": {"display_pin": 10, "bcm_pin": self.segment_pins[6]},  # top left segment
+                "tr": {"display_pin": 7, "bcm_pin": self.segment_pins[7]},  # top right segment
+                }
+
+        def define_number_map(self):
+            """
+            map numbers 0-9 to appropriate segments
+            """
+            self.number_map = {
+                    0: {"bl":0, "bm":0, "br":0, "mid":0, "tl":0, "tm":0, "tr":0, "dot":0},
+                    1: {"bl":0, "bm":0, "br":1, "mid":0, "tl":0, "tm":0, "tr":1, "dot":0},
+                    2: {"bl":1, "bm":1, "br":0, "mid":1, "tl":0, "tm":1, "tr":1, "dot":0},
+                    3: {"bl":0, "bm":1, "br":1, "mid":1, "tl":0, "tm":1, "tr":1, "dot":0},
+                    4: {"bl":0, "bm":0, "br":1, "mid":1, "tl":1, "tm":0, "tr":1, "dot":0},
+                    5: {"bl":0, "bm":1, "br":1, "mid":1, "tl":1, "tm":1, "tr":0, "dot":0},
+                    6: {"bl":1, "bm":1, "br":1, "mid":1, "tl":1, "tm":1, "tr":0, "dot":0},
+                    7: {"bl":0, "bm":0, "br":1, "mid":0, "tl":0, "tm":1, "tr":0, "dot":0},
+                    8: {"bl":1, "bm":1, "br":1, "mid":1, "tl":1, "tm":1, "tr":1, "dot":0},
+                    9: {"bl":0, "bm":1, "br":1, "mid":1, "tl":1, "tm":1, "tr":1, "dot":0},
+                    }
+
+        def output_digit(self, digit):
+            """
+            Handles GPIO segment output for the input digit
+            """
+            for k in self.segment_map.keys():
+                GPIO.output(self.segment_map[k]["bcm_pin"], self.number_map[digit][k]) 
+                time.sleep(0.001)
+
+        def output_digits(self, digits):
+            for i in range(len(digits)):
+                GPIO.output(self.digit_pins[i], 0) 
+                self.output_digit(digits[i])
+                GPIO.output(self.digit_pins[i], 1) 
+
+
---
+++ hx711.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+
+
+import RPi.GPIO as GPIO
+import time
+
+
+class HX711():
+    def __init__(self, data=22, clock=17, channel="A", gain=128):
+        """
+        Bit bangs data from HX711 using RPi.GPIO library.
+        The general logic for the HX711 is:
+            1) When the DATA pin goes low outside of a data reading loop,
+                that triggers the start of a new reading loop
+            2) There are 24 bits of data to be read inside a reading loop.
+                Each bit is read when CLOCK goes HIGH.
+            3) After the 24 bits are read there are 1-3 additional CLOCK
+                HIGH writes, which sets the channel and gain.
+
+        args:
+            DATA: (int) BCM pin# for DOUT
+            CLOCK: (int) BCM pin# for CLOCK
+            channel: (str) A (must be 64 or 128 gain) or B (32 gain)
+            gain: see channel comments
+        """
+        GPIO.setmode(GPIO.BCM)
+        self.DATA = data
+        self.CLOCK = clock
+        self.CHANNEL = channel
+        self.GAIN = gain
+        self.setup_pins()
+        self.setup_channel_gain()
+
+    def setup_pins(self, data=None, clock=None):
+        """
+        Defines the pins
+        """
+        GPIO.cleanup()
+
+        if data is not None:
+            self.DATA = data
+        if clock is not None:
+            self.CLOCK = clock
+
+        GPIO.setup(self.DATA, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(self.CLOCK, GPIO.OUT, initial=GPIO.LOW)
+        self._reset_state()
+        self._setup_events()
+
+    def _reset_state(self):
+        """
+        Sets state variables to their starting values
+        """
+        self.data_ready = False
+        self.raw_value = 0
+
+    def _setup_events(self):
+        """
+        Setup two event triggers:
+            1) When DATA goes low outside of a reading loop
+            2) When CLOCK goes low inside a reading loop
+        """
+        GPIO.add_event_detect(self.DATA, GPIO.FALLING)
+        GPIO.add_event_detect(self.CLOCK, GPIO.FALLING)
+    
+    def setup_channel_gain(self, channel=None, gain=None):
+        """
+        Create new variable determining number of additional CLOCK pulses,
+        depending on the channel and gain
+        """
+        if channel is not None:
+            assert channel in ["A", "B", "a", "b"]
+            self.CHANNEL = channel.upper()
+        if gain is not None:
+            assert gain in [32, 64, 128]
+            self.GAIN = gain
+
+        assert ((self.CHANNEL=="B") & (self.GAIN==32)) \
+                | ((self.CHANNEL=="A") & ((self.GAIN==64) | (self.GAIN==128)))
+        if self.CHANNEL == "B":        
+            self.EXTRA_PULSES = 2
+        elif self.GaIN == 64:
+            self.EXTRA_PULSES = 3
+        else:
+            self.EXTRA_PULSES = 1
+
+    def start_monitoring(self, printout=True):
+        try:
+            while True:
+                self.reset_state()
+                if (not self.data_ready) & (GPIO.event_detected(self.DATA)):
+                    # start the data reading process, using the CLOCK pin
+                    self.data_ready = True
+                    time.sleep(0.000001)
+                    for i in range(24):
+                        GPIO.output(self.CLOCK, GPIO.HIGH)
+                        time.sleep(0.000001)
+                        GPIO.output(self.CLOCK, GPIO.LOW)
+                        time.sleep(0.000001)
+                        bitval = GPIO.input(self.DATA)
+                        self.raw_value = (self.raw_value << 1) + bitval
+                    self.data_ready = False    
+                    if self.raw_value & 0x800000:  # unsigned to signed
+                        self.raw_value |= ~0xffffff
+                    if printout:    
+                        print("raw_value: {}".format(self.raw_value))    
+                    # Communicate the selected channel and gain settings
+                    for i in self.EXTRA_PULSES:
+                        GPIO.output(self.CLOCK, GPIO.HIGH)
+                        time.sleep(0.000001)
+                        GPIO.output(self.CLOCK, GPIO.LOW)
+
+        except KeyboardInterrupt:
+            pass
+
+        finally:
+            GPIO.cleanup()
+
+if __name__ == "__main__":
+    hx = HX711()
+    hx.start_monitoring
