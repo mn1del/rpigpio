68740fcc883e3d98f8e600eb8e2fd88d675a7eb9
mn1del <mn1del@users.noreply.github.com>
2019-03-12 15:55:59 +0000

    handle debouncing

--- toggle.py
+++ toggle.py
@@ -11,27 +11,46 @@ import time
 from rpigpio.base import BaseIO
 
 class Toggle(BaseIO):
-    def __init__(self, toggle_pin=4):
+    def __init__(self, toggle_pin=4, toggle_on_func=None, toggle_off_func=None, debounce_delay_secs=0.05):
         """
         Class to handle toggle switch input
         
         args:
             toggle_pin: (int) GPIO pin (BCM) for the encoder SW pin
+            toggle_on_func: optional function to call when toggle_on callback is triggered
+            toggle_off_func: optional function to call when toggle_off callback is triggered
+            debounce_delay_secs: (float) seconds delay to handle debouncing 
         """
         GPIO.setmode(GPIO.BCM)
         
         # define pin locations (BCM)
         self.TOGGLE = toggle_pin
         
-        # setup pins
-        GPIO.setup(self.TOGGLE, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-    def is_on(self):
+        # setup callbacks
+        self.DEBOUNCE_DELAY_SECS = debounce_delay_secs
+        self.LAST_DEBOUNCE_TIME = time.time()
+        
+        # read initial state:
+        self.STATE = GPIO.input(self.TOGGLE)
+        
+    def get_state(self):
         """
-        Return state of toggle switch
+        Return True or False depending on toggle state.
+        Includes some debouncing logic
         """
-        if self.TOGGLE:
-            state = True
-        else:
-            state = False
-        return state    
+        read_time = time.time()
+        
+        if (read_time - self.LAST_DEBOUNCE_TIME) < self.DEBOUNCE_DELAY_SECS:
+            time.sleep(self.DEBOUNCE_DELAY_SECS)
+        
+        # get reading
+        state = GPIO.input(self.TOGGLE)
+        
+        # handle change in state
+        if state != self.STATE:
+            self.LAST_DEBOUNCE_TIME = read_time
+            self.STATE = state
+        
+        return self.STATE
+                
+  
