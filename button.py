8c6dc96281514a78597e19b472e2a65230288b02
mn1del <mn1del@users.noreply.github.com>
2019-03-20 16:23:58 +0000

    add callback to update state

--- button.py
+++ button.py
@@ -11,30 +11,42 @@ import time
 from rpigpio.base import BaseIO
 
 class Button(BaseIO):
-    def __init__(self, button_pin=4, pull_down=False, debounce_delay_secs=0.05):
+    def __init__(self, 
+                 button_pin=4, 
+                 pull_up=True, 
+                 debounce_delay_secs=0.05):
         """
         Class to handle momentary switch input
         
         args:
             button_pin: (int) GPIO pin (BCM)
-            pull_down: (bool) if True set pull_up_down to GPIO.PUD_DOWN
+            pull_up: (bool) if True set pull_up_down to GPIO.PUD_UP
             debounce_delay_secs: (float) seconds delay to handle debouncing 
         """
         GPIO.setmode(GPIO.BCM)
         
-        # define pin locations (BCM)
-        self.BUTTON = toggle_pin
+        # set class variables
+        self.BUTTON = button_pin
+        self.DEBOUNCE_MS = debounce_delay_secs * 1000  # convert to milliseconds
+        self.CALLBACK_FUNC = callback_func
         
         # setup pins
-        GPIO.setup(self.BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-    def is_on(self, debounce_pause=0):
-        """
-        Return state of monentary switch
-        """
-        time.sleep(debounce_pause)
-        if self.BUTTON:
-            state = True
+        if pull_up:
+            GPIO.setup(self.BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+            self.STATE = True
         else:
-            state = False
-        return state    
+            GPIO.setup(self.BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+            self.STATE = False
+            
+        # setup event detection
+        GPIO.add_event_detect(self.BUTTON, GPIO.BOTH, callback=self.set_state, bouncetime=self.DEBOUNCE_MS)
+
+    def set_state(self, channel):
+        """
+        Sets and returns state using GPIO.event_detected() logic
+        """
+        time.sleep(self.DEBOUNCE_MS/1000)
+        state = GPIO.input(self.BUTTON)
+        if state != self.STATE:
+            self.STATE = state   
+            
