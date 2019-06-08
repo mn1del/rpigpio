f3d7e394573d49df3ece6dadc1db335b9f9b641d
mn1del <davidleech@gmail.com>
2019-06-08 15:12:53 +0100

    debug stepper.step()

--- stepper.py
+++ stepper.py
@@ -138,6 +138,7 @@ class Stepper(BaseIO):
             print("wake DRV8825")
             self.wake()
         GPIO.output(self.DIR, direction)
+        print("n_pauses: {}".format(len(step_pauses)))
         for step_pause in step_pauses:
             GPIO.output(self.STEP, GPIO.HIGH)
             GPIO.output(self.STEP, GPIO.LOW)
